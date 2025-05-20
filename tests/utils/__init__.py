ebe7c4393571558f696c671ce77621fa84e6fad6
Ridwan Faiz <rid1.fz.06@gmail.com>
2025-05-20 07:49:59 +0600

    tests: merge `create_file` and `create_empty_file` into single function

--- tests/utils/__init__.py
+++ tests/utils/__init__.py
@@ -10,7 +10,6 @@ __all__: list[str] = [
     "compare_files",
     "TestMKFile",
     "create_file",
-    "create_empty_file",
     "create_symlink",
     "get_random_str",
 ]
@@ -18,20 +17,16 @@ __all__: list[str] = [
 compare_files = functools.partial(filecmp.cmp, shallow=False)
 
 
-def create_file(path: pathlib.Path) -> None:
-    """Create a file with random string in it"""
+def create_file(path: pathlib.Path, *, empty: bool = False) -> None:
+    """Create a file in considering `path` as path to file"""
     path = path.absolute()
     path.parent.mkdir(parents=True, exist_ok=True)
 
-    with open(path, mode="w") as file:
-        file.write(get_random_str(random.randint(32, 256)))
-
-
-def create_empty_file(path: pathlib.Path) -> None:
-    """Create an empty file in `path`"""
-    path = path.absolute()
-    path.parent.mkdir(parents=True, exist_ok=True)
-    path.touch(exist_ok=False)
+    if empty:
+        path.touch(exist_ok=False)
+    else:
+        with open(path, mode="w") as file:
+            file.write(get_random_str(random.randint(32, 256), special_chars=True))
 
 
 def create_symlink(path: pathlib.Path, target: pathlib.Path) -> None:
